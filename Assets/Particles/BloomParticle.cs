49f647563a52c421b5ea911da1b65cf0486908fb
Kackbrise <kingstevefett@gmail.com>
2022-12-16 19:26:59 +0100

    particles

--- Assets/Particles/BloomParticle.cs
+++ Assets/Particles/BloomParticle.cs
@@ -6,11 +6,11 @@ using Terraria.ModLoader;
 
 namespace Divergency.Assets.Particles
 {
-	public class FlareLineParticle : Particle
+	public class BloomParticle: Particle
 	{
         public override string Texture => "Divergency/Assets/Textures/ParticleTextures/SoftCircle";
 
-        public override void SetDefaults()
+		public override void SetDefaults()
 		{
 			width = 1;
 			height = 1;
@@ -38,7 +38,7 @@ namespace Divergency.Assets.Particles
 			return false;
 		}
 	}
-    public class WindParticle : Particle
+    public class BloomParticleNPC: Particle
     {
         public override string Texture => "Divergency/Assets/Textures/ParticleTextures/SoftCircle";
 
@@ -46,7 +46,6 @@ namespace Divergency.Assets.Particles
         {
             width = 1;
             height = 1;
-            timeLeft = 260;
             opacity = 125;
             layer = Layer.BeforeNPCsBehindTiles;
         }
@@ -54,6 +53,59 @@ namespace Divergency.Assets.Particles
         public override void AI()
         {
             rotation = velocity.ToRotation();
+            NPC npc = Main.npc[(int)ai[0]];
+           // if (npc.type == ModContent.NPCType<WraithCore>() && npc.active)
+           if (npc.active)
+           {
+                position = npc.Center;
+                timeLeft = 10;
+           }
+            else
+            {
+               active = false;
+            }
+           
+            Scale = ai[0] == 0f ? 1f : ai[0];
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 drawPos, Color lightColor)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+            float alpha = timeLeft <= 20 ? 1f - 1f / 20f * (20 - timeLeft) : 1f;
+            if (alpha < 0f) alpha = 0f;
+            spriteBatch.Draw(texture, Center - Main.screenPosition, new Rectangle(0, 0, texture.Width, texture.Height), color, velocity.ToRotation(), new Vector2(texture.Width / 2f, texture.Height / 2f), 1f * Scale, SpriteEffects.None, 0f);
+
+            return false;
+        }
+    }
+    public class BloomParticleProjectile : Particle
+    {
+        public override string Texture => "Divergency/Assets/Textures/ParticleTextures/SoftCircle";
+
+        public override void SetDefaults()
+        {
+            width = 1;
+            height = 1;
+            opacity = 125;
+            layer = Layer.BeforeProjectiles;
+        }
+
+        public override void AI()
+        { 
+            rotation = velocity.ToRotation();
+            Projectile proj = Main.projectile[(int)ai[0]];
+            // if (proj.type == ModContent.ProjType<cocknballprojectile>() && proj.active)
+            if (proj.active)
+            {
+                position = proj.Center;
+                timeLeft = 10;
+
+            }
+            else
+            {
+                active = false;
+            }
 
             Scale = ai[0] == 0f ? 1f : ai[0];
         }
