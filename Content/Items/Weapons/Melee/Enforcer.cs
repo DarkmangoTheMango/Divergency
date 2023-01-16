d4493eaf3793a19f79ac8068363a6a6044f84e1a
Kackbrise <kingstevefett@gmail.com>
2023-01-16 21:31:20 +0100

    enforcer progress

--- Content/Items/Weapons/Melee/Enforcer.cs
+++ Content/Items/Weapons/Melee/Enforcer.cs
@@ -90,6 +90,8 @@ namespace Divergency.Content.Items.Weapons.Melee
 
             Projectile.aiStyle = -1;
             Projectile.ownerHitCheck = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 30;
         }
 
         List<float> oldRotation = new List<float>();
@@ -132,10 +134,13 @@ namespace Divergency.Content.Items.Weapons.Melee
 
             if (oldRotation.Count > 10) { oldRotation.RemoveAt(0); }
 
-            ParticleManager.NewParticle(player.Center + (Projectile.rotation.ToRotationVector2() * Main.rand.NextFloat(30f, 110f)), new Vector2(0f, Main.rand.NextFloat(1, 5)).RotatedBy(Projectile.rotation) * -SwingDirection, ParticleManager.NewInstance<StarParticle>(),
-                new Color(0.50f, 2f, 0.5f, 0), 0.5f, Projectile.whoAmI, Layer: Particle.Layer.BeforeNPCs);
+            //ParticleManager.NewParticle(player.Center + (Projectile.rotation.ToRotationVector2() * Main.rand.NextFloat(30f, 110f)), new Vector2(0f, Main.rand.NextFloat(1, 5)).RotatedBy(Projectile.rotation) * -SwingDirection, ParticleManager.NewInstance<StarParticle>(),
+              //  new Color(0.50f, 2f, 0.5f, 0), 0.5f, Projectile.whoAmI, Layer: Particle.Layer.BeforeNPCs);
         }
-
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), target.Center, Projectile.velocity.RotatedBy(6) * 10, ModContent.ProjectileType<EnforcerOrb>(), 20, 0, Projectile.owner);
+        }
         public override bool PreDraw(ref Color lightColor)
         {
             Player player = Main.player[Projectile.owner];
@@ -194,6 +199,115 @@ namespace Divergency.Content.Items.Weapons.Melee
             float collisionPoint = 0f;
 
             if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), player.Center, player.Center + ((96 * Projectile.scale) * Projectile.rotation.ToRotationVector2()), 20, ref collisionPoint)) { return true; }
+
+            return false;
+        }
+    }
+    public class EnforcerOrb : ModProjectile
+    {
+        public bool initialzed;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Life Orb");
+
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 2;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Generic;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.Size = new Vector2(14);
+            Projectile.scale = 1f;
+            Projectile.penetrate = 1;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = false;
+            Projectile.timeLeft = 600;
+            Projectile.aiStyle = -1;
+
+
+
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity *= 0.95f;
+            if (!initialzed)
+            {
+                initialDamage = Projectile.damage;
+
+                Projectile.damage = 0;
+            }
+
+            Player player = Main.player[Projectile.owner];
+            Main.NewText(initialzed);
+
+            if (Main.mouseRight && Main.mouseRightRelease && player.HeldItem.type == ModContent.ItemType<Enforcer>())
+            {
+                initialzed = true;
+                Projectile.damage = initialDamage;
+
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            
+            if (Projectile.active && Projectile.Hitbox.Intersects(player.Hitbox) && !player.dead && initialzed)
+            {
+                Projectile.Move(player.Center, 30);
+
+                player.Heal(2);
+                Projectile.Kill();
+                SoundEngine.PlaySound(SoundID.DD2_DarkMageHealImpact with { Volume = 0.8f, MaxInstances = 3 });
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Vector2 dir = Main.rand.NextVector2Unit() * 0.1f;
+
+                    ParticleManager.NewParticle(Projectile.Center, dir * Main.rand.NextFloat(10, 25), ParticleManager.NewInstance<StarParticle>(), new Color(0.50f, 2f, 0.5f, 0), 0.3f, Projectile.whoAmI, Layer: Particle.Layer.BeforeNPCs);
+
+                }
+
+            }
+            if (!Particlespawned)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    ParticleManager.NewParticle(Projectile.Center, new Vector2(0, 0), ParticleManager.NewInstance<BloomParticleProjectile>(), new Color(0.50f, 2f, 0.5f, 0), 0.03f, Projectile.whoAmI, Layer: Particle.Layer.BeforeNPCs);
+
+                }
+                Particlespawned = true;
+            }   
+
+        }
+
+        public Trail trail;
+        public Trail trail2;
+        private int initialDamage;
+
+        public bool Particlespawned { get; private set; }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+
+
+
+            Texture2D trailTexture = ModContent.Request<Texture2D>("Divergency/Assets/Textures/Trails/Stretched").Value;
+
+            if (trail == null)
+            {
+                trail = new Trail(trailTexture, Trail.DefaultPass, (p) => new Vector2(25f), (p) => Projectile.GetAlpha(new Color(30, 220, 30, 100)));
+                trail.drawOffset = Projectile.Size / 2f;
+            }
+            if (trail2 == null)
+            {
+                trail2 = new Trail(trailTexture, Trail.DefaultPass, (p) => new Vector2(10f), (p) => Projectile.GetAlpha(new Color(255, 255, 255, 100)));
+                trail2.drawOffset = Projectile.Size / 2f;
+            }
+
+            trail.Draw(Projectile.oldPos);
+            trail2.Draw(Projectile.oldPos);
 
             return false;
         }
