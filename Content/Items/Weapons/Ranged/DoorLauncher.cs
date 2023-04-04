714d626dd7776fd55d731ca919be600e20184f80
DarkMango <117542029+DarkWinters@users.noreply.github.com>
2023-04-04 14:39:01 -0600

    Living Core Spear Changes

---
+++ Content/Items/Weapons/Ranged/DoorLauncher.cs
@@ -0,0 +1,145 @@
+﻿using Divergency.Common.Helpers;
+using Divergency.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Divergency.Content.Items.Weapons.Ranged
+{
+    public class DoorLauncher : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Door Launcher");
+            Tooltip.SetDefault("Uses doors as ammo");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 40;
+            Item.crit = 3;
+            Item.knockBack = 4f;
+            Item.noMelee = true;
+
+            Item.useAmmo = AmmoID.Bullet;
+            Item.shoot = 1;
+            Item.shootSpeed = 10f;
+
+            Item.Size = new Vector2(58, 30);
+            Item.scale = 1f;
+
+            Item.useTime = Item.useAnimation = 35;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noUseGraphic = true;
+            Item.autoReuse = true;
+            Item.useTurn = false;
+
+            Item.value = Item.sellPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player == Main.LocalPlayer)
+            {
+                if (player.ItemAnimationActive) { player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, player.itemRotation - MathHelper.PiOver2 * player.direction); }
+                else { player.SetCompositeArmFront(false, default, default); }
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            float rotation = velocity.ToRotation();
+
+            Vector2 offset = new Vector2(0.8f, 0).RotatedBy(rotation);
+
+            for (int k = 0; k < 15; k++)
+            {
+                Vector2 direction = offset.RotatedByRandom(0.4f);
+                Dust.NewDustPerfect(position + offset * 70, ModContent.DustType<Glow>(), direction * Main.rand.NextFloat(8), 125, new Color(255, 108, 23), Main.rand.NextFloat(0.2f, 0.5f));
+            }
+
+            SoundEngine.PlaySound(SoundID.Item61, player.Center);
+
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<DoorLauncherPro>(), 0, 0f, player.whoAmI);
+
+            return false;
+        }
+    }
+
+    public class DoorLauncherPro : ModProjectile
+    {
+        bool initialize = true;
+
+        float maxTimeLeft;
+
+        public override string Texture => "Divergency/Content/Items/Weapons/Ranged/DoorLauncher";
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Door Launcher");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.scale = 1f;
+            Projectile.Size = Vector2.Zero;
+
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+
+            Projectile.aiStyle = -1;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - MathHelper.PiOver2);
+
+            if (initialize)
+            {
+                Projectile.timeLeft = player.HeldItem.useAnimation;
+                maxTimeLeft = Projectile.timeLeft;
+                Projectile.netUpdate = true;
+                initialize = false;
+            }
+
+            Projectile.Center = player.Center;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.Lerp(-0.5f * player.direction, 0f, EaseFunction.EaseQuinticOut.Ease(1 - (Projectile.timeLeft / maxTimeLeft)));
+
+            player.heldProj = Projectile.whoAmI;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Player player = Main.player[Projectile.owner];
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+            Rectangle sourceRectangle = texture.Frame(1, Main.projFrames[Projectile.type], frameY: Projectile.frame);
+            Vector2 origin = sourceRectangle.Size() / 2f;
+            Vector2 drawPosition = player.Center + Projectile.rotation.ToRotationVector2() * 10f - Main.screenPosition;
+
+            SpriteEffects drawFlipped = player.direction == -1 ? SpriteEffects.FlipVertically : 0;
+
+            Main.spriteBatch.Draw(texture, drawPosition, sourceRectangle, lightColor, Projectile.rotation, origin, Projectile.scale, drawFlipped, 0f);
+
+            return false;
+        }
+    }
+}
+
+ 
---
+++ Content/Projectiles/Ranged/Doors/WoodenDoor.cs
@@ -0,0 +1,84 @@
+using Divergency.Common.Players;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Divergency.Content.Projectiles.Ranged.Doors
+{
+    public class WoodenDoor : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Door");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.penetrate = 1;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.Size = new Vector2(48);
+            Projectile.scale = 1f;
+
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+
+            Projectile.aiStyle = -1;
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+
+            if (Projectile.ai[0] >= 30f)
+            {
+                Projectile.rotation += Projectile.velocity.Length() * 0.02f;
+
+                Projectile.velocity.Y += 0.5f;
+                Projectile.velocity.X *= 0.98f;
+            }
+            else
+            {
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            }
+
+            if (Projectile.velocity.Y >= 16f)
+            {
+                Projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Player player = Main.player[Projectile.owner];
+
+            SoundEngine.PlaySound(new SoundStyle("Divergency/Assets/Sounds/Impacts/WoodenDoorBreak") with { PitchVariance = 0.3f });
+
+            player.GetModPlayer<ScreenShakePlayer>().ScreenShakeIntensity = 5f;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+            int frameY = frameHeight * Projectile.frame;
+
+            Rectangle sourceRectangle = new Rectangle(0, frameY, texture.Width, frameHeight);
+            Vector2 origin = sourceRectangle.Size() / 2f;
+            Vector2 position = Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY);
+            Color color = Projectile.GetAlpha(lightColor);
+
+            SpriteEffects spriteEffects = SpriteEffects.None;
+
+            Main.EntitySpriteDraw(texture, position, sourceRectangle, color, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
+
+            return false;
+        }
+    }
+}
