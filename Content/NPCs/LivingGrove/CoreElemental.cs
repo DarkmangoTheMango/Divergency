e7bd79752581c8dd7230deffb363d0a9496fa40c
Kackbrise <kingstevefett@gmail.com>
2023-08-04 22:29:21 +0200

    core elemental start

---
+++ Content/NPCs/LivingGrove/CoreElemental.cs
@@ -0,0 +1,17 @@
+using Divergency.Content.Dusts;
+using Divergency.Content.Projectiles.Hostile;
+using Divergency.Content.Projectiles.Magic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ParticleLibrary;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Divergency.Content.NPCs.LivingGrove
+{
+    
+}
