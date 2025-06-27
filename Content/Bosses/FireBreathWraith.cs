9ceb8160a52a05855791b0a14646091fe1b77a12
Brise <kingstevefett@gmail.com>
2025-06-27 16:30:11 +0200

    began porting changes

---
+++ Content/Bosses/FireBreathWraith.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace Divergency.Content.Bosses
+{
+    public class FireBreathWraith : ModProjectile
+    {
+        public override Color? GetAlpha(Color lightColor) => new(255, 255, 255, 100);
+        public override string Texture => "Divergency/Assets/Textures/Empty";
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 15;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.penetrate = -1;
+            Projectile.DamageType = DamageClass.Generic;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.width = Projectile.height = 60;
+            Projectile.scale = 1f;
+            Projectile.penetrate = 3;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = false;
+            Projectile.alpha = 255;
+            Projectile.aiStyle = 0;
+            Projectile.timeLeft = 30;
+            Projectile.CritChance = 0;
+
+        }
+
+
+
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            if (Projectile.active && player.Hitbox.Intersects(Projectile.Hitbox))
+            {
+                //player.AddBuff(BuffID.DryadsWard, 180, true, false);
+
+            }
+            Vector3 RGB = new Vector3(1.45f, 2.55f, 0.94f);
+            float multiplier = 1f;
+            float max = 2f;
+            float min = 1f;
+            RGB *= multiplier;
+            if (RGB.X > max)
+            {
+                multiplier = 0.5f;
+            }
+            if (RGB.X < min)
+            {
+                multiplier = 0.6f;
+            }
+            Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+
+        }
+  
+    }
+}
