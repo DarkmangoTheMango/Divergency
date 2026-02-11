984e5103bf3dab2962d934f94055837c7bc382be
Darkmango <DarkmangoTheMango@gmail.com>
2026-02-10 21:30:40 -0800

    update Core Scorcher

--- Content/Items/Weapons/Magic/CoreScorcher.cs
+++ Content/Items/Weapons/Magic/CoreScorcher.cs
@@ -1,157 +1,197 @@
-using Divergency.Common.Helpers;
-using Divergency.Content.Dusts;
-using Divergency.Content.Particles;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using ParticleLibrary;
-using System;
-using Terraria;
+using Divergency.Content.Projectiles;
+using Divergency.Content.Projectiles.Ranged;
+using ReLogic.Utilities;
 using Terraria.Audio;
-using Terraria.DataStructures;
-using Terraria.GameContent.Creative;
-using Terraria.Graphics.Renderers;
-using Terraria.ID;
-using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
-namespace Divergency.Content.Items.Weapons.Magic
+namespace Divergency.Content.Items.Weapons.Magic;
+
+public class CoreScorcher : ModItem
 {
-    public class CoreScorcher : ModItem
+    public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] < 1;
+
+    public override void SetStaticDefaults()
     {
-
-
-        public override void SetStaticDefaults()
-        {
-            //.setdefault("Core Scorcher");
-
-            ////.setdefault("Fires a deadly Living Core flame"
-             //   + "\nInflicts the Dryads buffs/debuffs on enemy/player contact");
-
-            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-   
-
-        }
-
-        public override void SetDefaults()
-        {
-            Item.DamageType = DamageClass.Magic;
-            Item.damage = 25;
-            Item.knockBack = 1f;
-            Item.noMelee = true;
-            Item.crit = 0;
-            Item.shoot = ProjectileType<CoreFlame>();
-            Item.shootSpeed = 12f;
-            Item.mana = 2;
-            Item.width = Item.height = 16;
-            Item.scale = 1f;
-            Item.useTime = Item.useAnimation = 2;
-            Item.useStyle = ItemUseStyleID.Shoot;
-            
-            Item.autoReuse = true;
-            Item.useTurn = false;
-            
-            Item.value = Item.sellPrice(0, 2, 50, 0);
-            Item.rare = ItemRarityID.Green;
-        }
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            SoundEngine.PlaySound(SoundID.Item34 with { Volume = 1f, Pitch = Main.rand.NextFloat(0.5f, 2f), MaxInstances = 400 });
-
-            for (int i = 0; i < 5; i++)
-            {
-        
-                ParticleManager.NewParticle(position, velocity, ParticleManager.NewInstance<CoreFireParticle>(), Color.Purple, 0.85f);
-            }
-     
-                Dust dust = Dust.NewDustPerfect(position, ModContent.DustType<Smoke>(), new Vector2(Main.rand.NextFloat(-2f, 2f), Main.rand.NextFloat(-2f, -1f))* 5, 0, Color.LimeGreen, 0.9f);
-                dust.noGravity = true;
-
-                dust = Dust.NewDustPerfect(position, DustID.TerraBlade, Main.rand.NextVector2Circular(1f, 1f) * 5, 0, default, 2f);
-                dust.noGravity = true;
-            
-
-
-            return true;
-        }
-        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
-        {
-            Vector2 Offset = Vector2.Normalize(velocity) * 75f;
-            velocity = velocity.RotatedByRandom(MathHelper.ToRadians(5)) * (1f - Main.rand.NextFloat(0.35f));
-
-                position += Offset;
-            
-        }
-        public override Vector2? HoldoutOffset()
-        {
-            return new Vector2(-10, -5);
-        }
-    }
-
-    public class CoreFlame : ModProjectile
-    {
-        public override Color? GetAlpha(Color lightColor) => new(255, 255, 255, 100);
-        public override string Texture => "Divergency/Assets/Textures/Empty";
-
-        public override void SetStaticDefaults()
-        {
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 15;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
-        }
-        public override void SetDefaults()
-        {
-            Projectile.penetrate = -1;
-            Projectile.DamageType = DamageClass.Magic;
-            Projectile.friendly = true;
-            Projectile.hostile = false;
-            Projectile.width = Projectile.height = 60;
-            Projectile.scale = 1f;
-            Projectile.penetrate = 3;
-            Projectile.tileCollide = false;
-            Projectile.ignoreWater = false;
-            Projectile.alpha = 255;
-            Projectile.aiStyle = 0;
-            Projectile.timeLeft = 30;
-            Projectile.CritChance = 0;
-
-        }
-       
-
-
-
-        public override void AI()
-        {
-            Player player = Main.player[Projectile.owner];
-
-            if (Projectile.active && player.Hitbox.Intersects(Projectile.Hitbox))
-            {
-                player.AddBuff(BuffID.DryadsWard, 180, true, false);
-                
-            }
-            Vector3 RGB = new Vector3(1.45f, 2.55f, 0.94f);
-            float multiplier = 1f;
-            float max = 2f;
-            float min = 1f;
-            RGB *= multiplier;
-            if (RGB.X > max)
-            {
-                multiplier = 0.5f;
-            }
-            if (RGB.X < min)
-            {
-                multiplier = 0.6f;
-            }
-            Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
-
-        }
-         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            target.AddBuff(BuffID.DryadsWardDebuff, 120, false);
-        }
-
-
-     
 
     }
 
-    
+    public override void SetDefaults()
+    {
+        Item.DamageType = DamageClass.Magic;
+        Item.damage = 25;
+        Item.knockBack = 1f;
+        Item.noMelee = true;
+        Item.mana = 2;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.channel = true;
+        Item.value = Item.sellPrice(0, 2, 50, 0);
+        Item.rare = ItemRarityID.Green;
+
+        Item.Size = new(110, 40);
+
+        Item.shoot = ModContent.ProjectileType<CoreScorcherPro>();
+        Item.shootSpeed = 1;
+
+        Item.channel = true;
+        Item.noUseGraphic = true;
+        Item.useTime = Item.useAnimation = 5;
+        Item.reuseDelay = 10;
+        Item.useStyle = ItemUseStyleID.Shoot;
+
+        Item.UseSound = new SoundStyle("Divergency/Assets/Sounds/Items/CoreScorcherStart") with { PauseBehavior = PauseBehavior.PauseWithGame };
+
+        Item.value = Item.sellPrice(0, 5, 0, 0);
+        Item.rare = ItemRarityID.Green;
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        player.manaCost = 0f;
+
+        return true;
+    }
+}
+
+public class CoreScorcherPro : ModProjectile
+{
+    Player player => Main.player[Projectile.owner];
+
+    Vector2 shakeOffset;
+
+    SlotId soundSlot;
+
+    public override string Texture => "Divergency/Content/Items/Weapons/Magic/CoreScorcher";
+
+    public override bool ShouldUpdatePosition() => false;
+
+    public override bool? CanCutTiles() => false;
+
+    public override void SetStaticDefaults()
+    {
+
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.Size = new(110);
+
+        Projectile.DamageType = DamageClass.Melee;
+        Projectile.penetrate = -1;
+
+        Projectile.tileCollide = false;
+        Projectile.ignoreWater = true;
+
+        Projectile.aiStyle = -1;
+    }
+
+    public override void OnSpawn(IEntitySource source)
+    {
+        SoundEngine.PlaySound(SoundID.Item73, player.Center);
+        AI();
+    }
+    private float fade = 0f;
+    private bool dying = false;
+
+    public override void AI()
+    {
+        if (!player.CheckMana(2))
+            Projectile.Kill();
+
+        if (!player.channel && player.itemTime <= 2)
+            Projectile.Kill();
+
+        Projectile.direction = Projectile.Center.X > player.Center.X ? 1 : -1;
+
+        player.heldProj = Projectile.whoAmI;
+        player.ChangeDir(Projectile.direction);
+        Projectile.spriteDirection = Projectile.direction;
+
+        Projectile.velocity += (player.DirectionTo(Main.MouseWorld) - Projectile.velocity) * 0.2f;
+        Projectile.velocity = Projectile.velocity.SafeNormalize(Vector2.UnitX);
+        Projectile.Center = player.RotatedRelativePoint(player.MountedCenter, false, true) + Projectile.velocity * 45f;
+        Projectile.rotation = Projectile.velocity.ToRotation();
+
+        player.itemRotation = Projectile.rotation;
+
+        if (player.channel)
+            player.SetDummyItemTime(10);
+
+        player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, player.itemRotation * player.gravDir - MathHelper.PiOver2 + MathHelper.PiOver4 * player.direction);
+        player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, player.itemRotation * player.gravDir - MathHelper.PiOver2);
+
+        Lighting.AddLight(Projectile.Center, new Vector3(0, 1, 0) * 0.1f);
+        shakeOffset = Main.rand.NextVector2Circular(1, 1) * 2;
+
+        if (!SoundEngine.TryGetActiveSound(soundSlot, out _))
+        {
+            var tracker = new ProjectileAudioTracker(Projectile);
+
+            soundSlot = SoundEngine.PlaySound(new SoundStyle("Divergency/Assets/Sounds/Items/CoreScorcherLoop")
+            {
+                IsLooped = true,
+                Pitch = -5,
+                PauseBehavior = PauseBehavior.PauseWithGame,
+                SoundLimitBehavior = SoundLimitBehavior.ReplaceOldest
+            }, Projectile.Center, soundInstance =>
+            {
+                soundInstance.Position = Projectile.Center;
+
+                bool shouldBeActive = tracker.IsActiveAndInGame() && player.active && Projectile.active;
+
+                if (!shouldBeActive)
+                    dying = true;
+
+                if (dying)
+                    fade = MathHelper.Lerp(fade, 0f, 0.08f);
+                else
+                    fade = MathHelper.Lerp(fade, 1f, 0.08f);
+
+                soundInstance.Volume = fade;
+                soundInstance.Pitch = MathHelper.Lerp(soundInstance.Pitch, dying ? -1f : 0f, 0.1f);
+
+                return fade > 0.01f;
+            });
+        }
+
+        if (++Projectile.ai[0] >= 3)
+        {
+            player.CheckMana(2, true);
+
+            Projectile.ai[0] = 0;
+
+
+            Vector2 position = player.Center;
+
+            if (Collision.CanHit(position, 16, 16, Projectile.Center + Projectile.velocity * 45f, 16, 16))
+                position = Projectile.Center + Projectile.velocity * 45f;
+
+            Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), position, Projectile.velocity.RotatedByRandom(0.2f) * 20, ModContent.ProjectileType<CoreFlame>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+        }
+    }
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+        Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+        Vector2 drawPosition = Projectile.Center - Main.screenPosition + shakeOffset;
+
+        int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+        int startY = frameHeight * Projectile.frame;
+        Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
+
+        Color color = Projectile.GetAlpha(lightColor);
+
+        Vector2 drawOrigin = sourceRectangle.Size() / 2;
+
+        SpriteEffects spriteEffects = Projectile.spriteDirection > 0 ? SpriteEffects.None : SpriteEffects.FlipVertically;
+
+        Main.EntitySpriteDraw(texture, drawPosition, sourceRectangle, color, Projectile.rotation, drawOrigin, Projectile.scale, spriteEffects, 0);
+
+        texture = ModContent.Request<Texture2D>(Texture + "_Glow").Value;
+
+        color = Projectile.GetAlpha(Color.White);
+
+        Main.EntitySpriteDraw(texture, drawPosition, sourceRectangle, color, Projectile.rotation, drawOrigin, Projectile.scale, spriteEffects, 0);
+
+        return false;
+    }
 }
---
+++ Content/Projectiles/Ranged/CoreFlame.cs
@@ -0,0 +1,156 @@
+﻿using Divergency.Content.Particles;
+using Microsoft.Build.ObjectModelRemoting;
+using ParticleLibrary;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.Audio;
+
+namespace Divergency.Content.Projectiles.Ranged;
+
+public class CoreFlame : ModProjectile
+{
+    private int rotationDirection = 1;
+
+    public override bool OnTileCollide(Vector2 oldVelocity)
+    {
+        doesntFollow = true;
+        return false;
+    }
+
+    public override void SetStaticDefaults()
+    {
+        Main.projFrames[Projectile.type] = 7;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.Size = new(98);
+        Projectile.scale = 0.1f;
+        Projectile.DamageType = DamageClass.Magic;
+        Projectile.friendly = true;
+        Projectile.scale = 1f;
+        Projectile.penetrate = 3;
+        Projectile.ignoreWater = false;
+        Projectile.aiStyle = -1;
+        AIType = -1;
+        Projectile.stopsDealingDamageAfterPenetrateHits = true;
+    }
+
+    public override void OnSpawn(IEntitySource source)
+    {
+        Projectile.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+    }
+
+    public override void OnKill(int timeLeft)
+    {
+
+    }
+
+    #region AI
+
+    Player player => Main.player[Projectile.owner];
+
+    bool doesntFollow = false;
+
+    public override void AI()
+    {
+        if (Main.rand.NextBool((int)Projectile.ai[0] + 1) && Projectile.alpha < 128)
+            ParticleManager.NewParticle<CoreSparkle>(Projectile.Center + Main.rand.NextVector2Circular(1, 1) * (Projectile.width * 0.5f * (Projectile.scale * 0.5f)), Projectile.velocity * 0.1f, Color.White, 1);
+
+        Projectile.ai[0]++;
+
+        UpdateFrame(2);
+
+        Projectile.scale += 0.1f;
+
+        if (Projectile.alpha >= 255)
+            Projectile.Kill();
+
+        Projectile.alpha += 12;
+
+        if (Projectile.alpha > 128)
+            Projectile.damage = 0;
+
+        Projectile.Resize((int)(98 * Projectile.scale), (int)(98 * Projectile.scale));
+
+        if (!doesntFollow)
+            Projectile.Center += player.velocity;
+        else
+            Projectile.velocity *= 0.9f;
+    }
+
+    void UpdateFrame(int frameSpeed)
+    {
+        if (++Projectile.frameCounter > frameSpeed)
+        {
+            Projectile.frameCounter = 0;
+            Projectile.frame++;
+
+            if (Projectile.frame > Main.projFrames[Projectile.type] - 1)
+                Projectile.Kill();
+        }
+    }
+
+    #endregion AI
+
+    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+    {
+        width = 16;
+        height = 16;
+
+        return true;
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        Main.player[Projectile.owner].AddBuff(BuffID.DryadsWard, 60);
+    }
+
+    #region Drawing
+
+    public override bool PreDraw(ref Color lightColor)
+    {
+        DrawBloom();
+
+        Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+        int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+        Rectangle sourceRectangle = new(0, frameHeight * Projectile.frame, texture.Width, frameHeight);
+
+        Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, sourceRectangle, Projectile.GetAlpha(new(255, 255, 255, 128)), Projectile.rotation, sourceRectangle.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
+
+        return false;
+    }
+
+    void DrawBloom()
+    {
+        Texture2D texture = ModContent.Request<Texture2D>("Divergency/Assets/Textures/Bloom2").Value;
+
+        Rectangle sourceRectangle = new(0, 0, texture.Width, texture.Height);
+
+        Main.spriteBatch.End();
+        Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+
+        Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, sourceRectangle, Projectile.GetAlpha(new(96, 214, 72)), Projectile.rotation, sourceRectangle.Size() * 0.5f, Projectile.scale * 0.5f, SpriteEffects.None, 0);
+
+        Main.spriteBatch.End();
+        Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+    }
+
+    #endregion Drawing
+
+    #region Networking
+
+    public override void SendExtraAI(BinaryWriter writer)
+    {
+
+    }
+
+    public override void ReceiveExtraAI(BinaryReader reader)
+    {
+
+    }
+
+    #endregion Networking
+}
